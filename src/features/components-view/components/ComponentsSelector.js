73196536dfa7ddf83a2ba35e7bad5e4dbd1321e7
BL <iceliang@gmail.com>
2020-07-02 23:11:20 +1200

    Replaced "selected" with "value prop" in <select>

--- src/features/components-view/components/ComponentsSelector.js
+++ src/features/components-view/components/ComponentsSelector.js
@@ -4,15 +4,15 @@ class ComponentsSelector extends React.Component {
 	render() {
 		return (
 			<Fragment>
-				<select name='category' onChange={this.props.onCategoryChange}>
+				<select
+					name='category'
+					value={this.props.currentCategory}
+					onChange={this.props.onCategoryChange}>
 					{this.props.optionGroups
 						.get("currentCategory")
 						.map((option, index) => {
 							return (
-								<option
-									key={index}
-									value={option[0]}
-									selected={this.props.currentCategory === option[0]}>
+								<option key={index} value={option[0]}>
 									{option[1]}
 								</option>
 							);
